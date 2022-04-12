97ac7b06853a266644b2f3a1c2cb93ebcd11b1fc
Person11111111 <87883601+Person11111111@users.noreply.github.com>
2022-04-12 19:33:18 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,7 @@
+console.log('Connected')
+function addUser(){
+    uname = document.getElementById('uname').value;
+    localStorage.setItem('myUnAme', uname)
+    window.location = 'C:\\Users\\Hamzah2030\\Desktop\\kwitter project\\home_page\\main_room_interface.html'
+}
+© 2022 GitHub, Inc.
